a3c80bcc40dd82ca289a41306b3be97a7a8c5e85
Aitor Urrutia <aitor.urrutia@biko2.com>
2021-10-22 08:59:00 +0200

    chore: quita boilerplate inicial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Pokédex', () => {
+  it('renders the component', () => {
+    render(<App />)
+
+    expect(screen.getByText(/aquí irán los pokemon/i)).toBeInTheDocument()
+  })
+})
